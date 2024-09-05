9af58db4b7d76a2049b3eaf9599df2a53188dd9c
Prafull Prince <prafullprince@Prafulls-Mac-mini.local>
2024-09-05 16:08:35 +0530

    update todo

--- controllers/Todo.js
+++ controllers/Todo.js
@@ -26,8 +26,39 @@ exports.createTodo = async (req,res)=>{
         });
     }
 }
+
 // update todo
+exports.updateTodo = async (req,res)=>{
+    try{
+        // fetch todo id
+        const {id} = req.params;
+        const {title,description} = req.body;
+        // search and update in db 
+        const data = await Todo.findByIdAndUpdate(
+            {_id:id},
+            {
+                title:title,
+                description:description,
+            },
+            {new:true}
+        );
+        return res.status(200).json({
+            success:true,
+            data:data,
+            message:"todo updated successfully"
+        });
+
+    } catch(error){
+        console.log(error);
+        return res.status(500).json({
+            success:true,
+            message:"something went wrong during creating todo"
+        });
+    }
+}
+
 // delete todo
+
 // get all todo
 exports.getAllTodo = async (req,res)=>{
     try{
--- routes/todos.js
+++ routes/todos.js
@@ -4,10 +4,13 @@ const express = require("express");
 const router = express.Router();
 
 // import login according to routes
-const { createTodo } = require("../controllers/Todo");
+const { createTodo, getAllTodo, getTodo, updateTodo } = require("../controllers/Todo");
 
 // make api routes
 router.post('/createTodo',createTodo);
+router.get('/getAllTodo',getAllTodo);
+router.get('/getTodo/:id',getTodo);
+router.put('/updateTodo/:id',updateTodo);
 
 // export router
 module.exports = router;
