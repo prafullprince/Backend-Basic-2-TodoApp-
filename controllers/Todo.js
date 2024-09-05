f7dd32268fab6fc09bab45780bdb27193b297fe7
Prafull Prince <prafullprince@Prafulls-Mac-mini.local>
2024-09-05 15:54:53 +0530

    getAll Todo and getSingleTodo

--- controllers/Todo.js
+++ controllers/Todo.js
@@ -28,4 +28,44 @@ exports.createTodo = async (req,res)=>{
 }
 // update todo
 // delete todo
-// get todo
+// get all todo
+exports.getAllTodo = async (req,res)=>{
+    try{
+        // search in db 
+        const data = await Todo.find({}).sort({createdAt:-1});
+        return res.status(200).json({
+            success:true,
+            data:data,
+            message:"todo fetched successfully"
+        });
+
+    } catch(error){
+        console.log(error);
+        return res.status(500).json({
+            success:true,
+            message:"something went wrong during creating todo"
+        });
+    }
+}
+
+// get single todo
+exports.getTodo = async (req,res)=>{
+    try{
+        // fetch todo id
+        const {id} = req.params;
+        // search in db 
+        const data = await Todo.findById({_id:id});
+        return res.status(200).json({
+            success:true,
+            data:data,
+            message:"todo fetched successfully"
+        });
+
+    } catch(error){
+        console.log(error);
+        return res.status(500).json({
+            success:true,
+            message:"something went wrong during creating todo"
+        });
+    }
+}
